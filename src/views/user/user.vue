1d3837fd47e89227e6c8b5f42c5b865f98ba0f3a
rayrain13 <dirtin3@outlook.com>
2024-11-17 11:55:50 +0800

    fix:个人中心页面

--- src/views/user/user.vue
+++ src/views/user/user.vue
@@ -1,10 +1,38 @@
 <template>
-  <div></div>
+  <div id="user">
+    <userBox v-show="userList" @userBoxData="getUserBox"></userBox>
+    <userInfo v-show="userInfo" @userInfoData="getUserInfo"></userInfo>
+  </div>
 </template>
 
-<script setup>
+<script>
+  import userBox from "../../compoment/user/userBox";
+  import userInfo from "../../compoment/user/userInfo";
 
+export default {
+  name: "user",
+  components: { userBox, userInfo },
+  data() {
+    return {
+      userList: true,
+      userInfo: false
+    };
+  },
+  methods: {
+    //父子组件传值
+    getUserBox(childData) {
+      this.userList = childData;
+      this.userInfo = true;
+    },
+    getUserInfo(childData) {
+      console.log(childData);
+      this.userInfo = childData;
+      this.userList = true;
+    }
+  }
+};
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+
 </style>
